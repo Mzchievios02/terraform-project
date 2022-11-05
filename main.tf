0e0b17df92e9b254db1929f441dcfc1e4d5459f3
tee <t.odigie1@gmail.com>
2022-11-05 11:39:30 -0600

    Add variable and output

--- main.tf
+++ main.tf
@@ -11,11 +11,11 @@ resource "aws_instance" "this" {
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p ${var.server_port} &
               EOF
 
   user_data_replace_on_change = true
-  
+
   tags = {
     "Name" = "terraform-example"
   }
@@ -26,8 +26,19 @@ resource "aws_security_group" "instance" {
 
   ingress {
     cidr_blocks = ["0.0.0.0/0"]
-    from_port = 8080
+    from_port = var.server_port
     protocol = "tcp"
-    to_port = 8080
+    to_port = var.server_port
   }
+}
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type = number
+  default = 8080
+}
+
+output "public_ip" {
+  value = aws_instance.this.public_ip
+  description = "The public IP address of the web server"
 }
