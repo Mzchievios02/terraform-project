3b45d155d52b0d4dae48833b84ebde9dc5307b05
tee <t.odigie1@gmail.com>
2022-11-12 16:43:38 -0700

    Use templatefile for userdata

--- 03b-isolate-envs/stage/services/webserver-cluster/main.tf
+++ 03b-isolate-envs/stage/services/webserver-cluster/main.tf
@@ -1,19 +1,19 @@
 provider "aws" {
-  region = "us-east-2"
+  region  = "us-east-2"
   profile = "personal"
 }
 
 resource "aws_launch_configuration" "this" {
-  image_id = "ami-0fb653ca2d3203ac1"
-  instance_type = "t2.micro"
-  security_groups  = [aws_security_group.instance.id]
+  image_id        = "ami-0fb653ca2d3203ac1"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
 
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p ${var.server_port} &
-              EOF
-  
+  user_data = templatefile("user-data.sh", {
+    server_port = var.server_port
+    db_address  = data.terraform_remote_state.db.outputs.address
+    db_port     = data.terraform_remote_state.db.outputs.port
+  })
+
   # Required when using a launch configuration with an auto scaling group
   lifecycle {
     create_before_destroy = true
@@ -22,43 +22,43 @@ resource "aws_launch_configuration" "this" {
 }
 
 resource "aws_autoscaling_group" "this" {
-    launch_configuration = aws_launch_configuration.this.name
-    vpc_zone_identifier = data.aws_subnets.default.ids
+  launch_configuration = aws_launch_configuration.this.name
+  vpc_zone_identifier  = data.aws_subnets.default.ids
 
-    target_group_arns = [aws_lb_target_group.asg.arn]
-    health_check_type = "ELB"
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
 
-    max_size = 5
-    min_size = 2
+  max_size = 5
+  min_size = 2
 
-    tag {
-        key = "Name"
-        value = "terraform-asg-example"
-        propagate_at_launch = true
-    }
-  
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+
 }
 
 resource "aws_lb" "this" {
-  name = "aws-lb-terraform"
+  name               = "aws-lb-terraform"
   load_balancer_type = "application"
-  subnets = data.aws_subnets.default.ids
-  security_groups = [aws_security_group.alb.id]
+  subnets            = data.aws_subnets.default.ids
+  security_groups    = [aws_security_group.alb.id]
 }
 
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.this.arn
-  port = 80
-  protocol = "HTTP"
+  port              = 80
+  protocol          = "HTTP"
 
   # Retrun 404 if the requests don't match port80
   default_action {
     type = "fixed-response"
 
     fixed_response {
-    content_type = "text/plain"
-    message_body = "404: page not found"
-    status_code = 404
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
     }
   }
 }
@@ -68,50 +68,50 @@ resource "aws_security_group" "alb" {
 
   # Allow inbound HTTP requests
   ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   # Allow outbound requests
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 resource "aws_lb_target_group" "asg" {
-  name = "terraform-asg-example"
-  port = var.server_port
+  name     = "terraform-asg-example"
+  port     = var.server_port
   protocol = "HTTP"
-  vpc_id = data.aws_vpc.default.id
+  vpc_id   = data.aws_vpc.default.id
 
   health_check {
-    path = "/"
-    protocol = "HTTP"
-    matcher = "200"
-    interval = 15
-    timeout = 3
-    healthy_threshold = 2
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
     unhealthy_threshold = 2
   }
 }
 
 resource "aws_lb_listener_rule" "asg" {
   listener_arn = aws_lb_listener.http.arn
-  priority = 100
+  priority     = 100
 
   condition {
     path_pattern {
-        values = ["*"]
+      values = ["*"]
     }
   }
   action {
-    type = "forward"
-    target_group_arn =  aws_lb_target_group.asg.arn
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
   }
 }
 resource "aws_security_group" "instance" {
@@ -119,28 +119,39 @@ resource "aws_security_group" "instance" {
 
   ingress {
     cidr_blocks = ["0.0.0.0/0"]
-    from_port = var.server_port
-    protocol = "tcp"
-    to_port = var.server_port
+    from_port   = var.server_port
+    protocol    = "tcp"
+    to_port     = var.server_port
   }
 }
 
 data "aws_vpc" "default" {
-    default = true
+  default = true
 }
 
 data "aws_subnets" "default" {
   filter {
-    name = "vpc-id"
+    name   = "vpc-id"
     values = [data.aws_vpc.default.id]
+  }
+}
+
+data "terraform_remote_state" "db" {
+  backend = "s3"
+
+  config = {
+    bucket = "tf-state-toks"
+    key    = "stage/data-stores/mysql/terraform.state"
+    region = "us-east-2"
+    profile = "personal"
   }
 }
 
 terraform {
   backend "s3" {
-    bucket = "tf-state-toks"
-    key    = "workspaces/terraform.state"
-    region = "us-east-2"
+    bucket  = "tf-state-toks"
+    key     = "stage/services/webserver-cluster/terraform.tfstate"
+    region  = "us-east-2"
     profile = "personal"
 
     dynamodb_table = "terraform-locks"
