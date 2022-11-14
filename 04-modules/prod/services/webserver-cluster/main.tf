ecea492dd180f4abf2eef98a2d4651ed2eced427
tee <t.odigie1@gmail.com>
2022-11-13 23:29:47 -0700

    fmt files

--- 04-modules/prod/services/webserver-cluster/main.tf
+++ 04-modules/prod/services/webserver-cluster/main.tf
@@ -5,31 +5,31 @@ provider "aws" {
 module "webserver_cluser" {
   source = "../../../../modules/services/webserver-cluster"
 
-  cluster_name = "webservers-prod"
+  cluster_name           = "webservers-prod"
   db_remote_state_bucket = "tf-state-toks"
-  db_remote_state_key = "prod/data-stores/mysql/terraform.state"
+  db_remote_state_key    = "prod/data-stores/mysql/terraform.state"
 
   instance_type = "t2.micro"
-  min_size = 2
-  max_size = 2
+  min_size      = 2
+  max_size      = 2
 }
 
 resource "aws_autoscaling_schedule" "scale_out_during_business_hours" {
   scheduled_action_name = "scale_out_during_business_hours"
-  min_size = 2
-  max_size = 10
-  desired_capacity = 10
-  recurrence = "0 9 * * *"
+  min_size              = 2
+  max_size              = 10
+  desired_capacity      = 10
+  recurrence            = "0 9 * * *"
 
   autoscaling_group_name = module.webserver_cluser.asg_name
 }
 
 resource "aws_autoscaling_schedule" "scale_in_at_night" {
   scheduled_action_name = "scale_in_at_night"
-  min_size = 2
-  max_size = 10
-  desired_capacity = 2
-  recurrence = "0 17 * * *"
+  min_size              = 2
+  max_size              = 10
+  desired_capacity      = 2
+  recurrence            = "0 17 * * *"
 
   autoscaling_group_name = module.webserver_cluser.asg_name
 }
